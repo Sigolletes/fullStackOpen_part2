80867e3ebe72ccf068cef5e006c42899f87abc07
Sigolletes <juditreverter87@gmail.com>
2022-12-04 14:25:13 +0100

    Add separate module to courseinfo2

--- courseinfo2/src/App.js
+++ courseinfo2/src/App.js
@@ -1,16 +1,4 @@
-const Course = ({ course }) => {
-  return (
-    <div>
-      <h2>{course.name}</h2>
-      {course.parts.map(part => 
-        <p key={part.id}>{part.name} {part.exercises}</p>
-      )}
-      <h3>Total: {course.parts.reduce(
-        (total, current) => total + current.exercises, 0
-      )} exercises</h3>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
---
+++ courseinfo2/src/components/Course.js
@@ -0,0 +1,15 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <h2>{course.name}</h2>
+      {course.parts.map(part => 
+        <p key={part.id}>{part.name} {part.exercises}</p>
+      )}
+      <h3>Total: {course.parts.reduce(
+        (total, current) => total + current.exercises, 0
+      )} exercises</h3>
+    </div>
+  )
+}
+
+export default Course
