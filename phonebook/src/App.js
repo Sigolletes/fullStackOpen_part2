660045eb22d5108f4693c147fcedb1a6a005c9f4
Sigolletes <juditreverter87@gmail.com>
2022-12-20 13:12:47 +0100

    Add phonebook step4

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -31,9 +35,37 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const renderNumbers = (event) => {
+    if (filter.length > 0) {
+      let reg = new RegExp(`^${filter}` , 'i')
+
+      let filtered = persons.filter(person => person.name.match(reg))
+      return filtered.map(person =>
+        <p key={person.name}>{person.name}: {person.number}</p>
+      )
+    } else {
+      return persons.map(person => 
+        <p key={person.name}>{person.name}: {person.number}</p> 
+      )
+    }
+  }
+
   return (
     <div>
       <h2>Phonebook</h2> 
+
+        <div>
+          Filter shown with: <input 
+              type='text'
+              value={filter}
+              onChange={handleFilter}
+            />
+        </div>
+
       <form onSubmit={addPerson}>
         <div>
           Name: <input 
@@ -45,16 +77,14 @@ const App = () => {
           Number: <input 
               value={newNumber} 
               onChange={handleNumberChange}
-              />
+            />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => 
-        <p key={person.name}>{person.name}: {person.number}</p> 
-        )}
+        {renderNumbers()}
     </div>
   )
 }
