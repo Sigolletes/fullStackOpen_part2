30e21b3856114295d062b33f1b093e0eae409afe
Sigolletes <juditreverter87@gmail.com>
2023-01-24 10:21:17 +0100

    Delete unnecessary comments

--- phonebook/src/index.js
+++ phonebook/src/index.js
@@ -4,13 +4,4 @@ import './index.css'
 
 import App from './App'
 
-/* import axios from 'axios' */
-
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-
-/* axios.get('http://localhost:3001/persons').then(response => {
-  const persons = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App persons={persons} />
-  )
-}) */
